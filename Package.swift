9827d01b1015442a936007d780e877c10066bf9a
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2021-09-07 17:07:06 +0700

    Update dependency for swift package manager

--- Package.swift
+++ Package.swift
@@ -4,27 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "QKMRZScanner",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "QKMRZScanner",
-            targets: ["QKMRZScanner"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "QKMRZScanner",
-            dependencies: [],
-            path: "QKMRZScanner"),
-        .testTarget(
-            name: "QKMRZScannerTests",
-            dependencies: ["QKMRZScanner"],
-            path: "Tests"),
-    ]
+  name: "QKMRZScanner",
+  products: [
+    .library(
+      name: "QKMRZScanner",
+      targets: ["QKMRZScanner"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Mattijah/QKMRZParser", from: "1.0.0"),
+    .package(url: "https://github.com/SwiftyTesseract/SwiftyTesseract", from: "4.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "QKMRZScanner",
+      dependencies: ["QKMRZParser", "SwiftyTesseract"],
+      path: "QKMRZScanner"),
+    .testTarget(
+      name: "QKMRZScannerTests",
+      dependencies: ["QKMRZScanner"],
+      path: "Tests"),
+  ]
 )
